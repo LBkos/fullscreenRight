fd91ec7f8bde8d09f5e767d13f24f239b23b2008
LBkos <constantin.l.o.p@gmail.com>
2023-10-23 20:44:23 +0700

    Initial Commit

---
+++ Ex/ExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExApp.swift
+//  Ex
+//
+//  Created by Константин Лопаткин on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
